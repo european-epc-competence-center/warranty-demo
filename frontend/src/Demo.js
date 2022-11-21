3dbbbe3e35fd56cd69feb6d46bad2dd42ab2fb94
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-11-21 16:14:08 +0100

    skip buttons

--- frontend/src/Demo.js
+++ frontend/src/Demo.js
@@ -429,7 +429,7 @@ const MainAccordion = props => {
                   </div>
                   <div className='d-flex justify-content-center'>
                     <NextStateButton
-                      currentState={props.demoState.state}
+                      next_state='EBON_PRESENTATION_REQUEST_SENT_FROM_MANUFACTURER'
                       label='Skip'
                       force={true}
                       demoUserID={props.demoUserID}
@@ -466,13 +466,13 @@ const MainAccordion = props => {
                     requesting party.
                   </p>
                   <p>
-                    Notice that no personal data is queried. Instead, you present a proof 
+                    Notice that no personal data is queried. Instead, you present a proof
                     that you bought exactly this item and information about when and where it was bought.
                     Even more, you only partially disclose the ebon, keeping the price of the purchase secret.
                   </p>
                   <div className='d-flex justify-content-center'>
                     <NextStateButton
-                      currentState={props.demoState.state}
+                      next_state='PRODUCT_CERTIFICATE_CREDENTIAL_OFFER_SENT_BY_MANUFACTURER'
                       label='Skip'
                       force={true}
                       demoUserID={props.demoUserID}
@@ -591,12 +591,20 @@ const MainAccordion = props => {
                     data and decide whether to answer the presentation request.
                   </p>
                   <p>
-                    Additionally, you can 
+                    Additionally, you can
                     description of the mal function directly with the warranty presentation.
                     Using this feature, you can get information directly from a customer
                     without the need of creating an account and logging him into a website.
                     The proof guarantees that the information entered stems from the warranty certificate holder.
                   </p>
+                  <div className='d-flex justify-content-center'>
+                    <NextStateButton
+                      currentState={props.demoState.state}
+                      label='Skip'
+                      force={true}
+                      demoUserID={props.demoUserID}
+                    />
+                  </div>
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey='1'>
@@ -687,17 +695,17 @@ const Footer = props => {
         </a>
         <h6>European EPC Competence Center</h6>
         <img
-            src={bmwk_logo}
-            height='150'
-            alt='EECC Logo'
-            style={{ margin: '10px' }}
-          />
+          src={bmwk_logo}
+          height='150'
+          alt='EECC Logo'
+          style={{ margin: '10px' }}
+        />
       </div>
       <div className='d-flex justify-content-center align-items-center my-5'>
         <small>This project is open source on </small>
         <a href="https://github.com/european-epc-competence-center/warranty-demo">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6795d0" class="bi bi-github ms-1" viewBox="0 0 16 16">
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
           </svg>
         </a>
       </div>
