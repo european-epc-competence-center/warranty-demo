31a2f96ba68ed5de9ba88de79cb689bfb0e019c6
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-11-21 16:58:21 +0100

    fixes next state button, added all skips

--- frontend/src/Demo.js
+++ frontend/src/Demo.js
@@ -8,6 +8,7 @@ import axios from 'axios'
 import QRCode from 'react-qr-code'
 import Accordion from 'react-bootstrap/Accordion'
 import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -601,6 +602,7 @@ const MainAccordion = props => {
                     <NextStateButton
                       currentState={props.demoState.state}
                       label='Skip'
+                      steps={3}
                       force={true}
                       demoUserID={props.demoUserID}
                     />
@@ -628,6 +630,15 @@ const MainAccordion = props => {
                     can present your address data from your base ID credential.
                     This step demonstrates how personaldata can be imported from your wallet if and only if you consent.
                   </p>
+                  <div className='d-flex justify-content-center'>
+                    <NextStateButton
+                      currentState={props.demoState.state}
+                      label='Skip'
+                      steps={1}
+                      force={true}
+                      demoUserID={props.demoUserID}
+                    />
+                  </div>
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey='2'>
@@ -656,6 +667,11 @@ const MainAccordion = props => {
                     </li>
                   </ul>
 
+                  <div className='d-flex justify-content-center'>
+                    <Button variant='secondary' onClick={reloadPage} className="m-2">
+                      Restart Demo
+                    </Button>
+                  </div>
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
@@ -664,6 +680,10 @@ const MainAccordion = props => {
       </Accordion>
     </Container>
   )
+}
+
+const reloadPage = () => {
+  window.location.reload()
 }
 
 const DisplayCheckmark = props => {
@@ -704,7 +724,7 @@ const Footer = props => {
       <div className='d-flex justify-content-center align-items-center my-5'>
         <small>This project is open source on </small>
         <a href="https://github.com/european-epc-competence-center/warranty-demo">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6795d0" class="bi bi-github ms-1" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#6795d0" className="bi bi-github ms-1" viewBox="0 0 16 16">
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
           </svg>
         </a>
--- frontend/src/NextStateButton.js
+++ frontend/src/NextStateButton.js
@@ -4,17 +4,18 @@ import { BACKEND_URL, STORY_LINE } from './Config'
 
 const NextStateButton = props => {
     function setDemoStateAtBackend() {
-        var next_state = ""
+        var nextState = ""
         if (typeof props.nextState !== 'undefined') {
-            next_state = props.nextState
+            nextState = props.nextState
         } else if (typeof props.currentState !== 'undefined') {
             const steps = props.steps || 1
             let current_state_index = STORY_LINE.indexOf(props.currentState)
             let next_state_index = (current_state_index + steps) % STORY_LINE.length
-            var nextState = STORY_LINE[next_state_index]
+            nextState = STORY_LINE[next_state_index]
             console.log("nextState: " + nextState)
         } else {
             console.log("Error in NextButton call: Neither nextState nor currenState given")
+            return;
         }
 
         var params = { nextState: nextState, demo_user_id: props.demoUserID }
